efb3502e7ef502234bb3e0f37c9cb500e86fd334
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2021-02-20 21:48:35 -0300

    atualização do projeto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from'./view/home/home.component';
+import {ProductCrudComponent } from'./view/product-crud/product-crud.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path:"",
+  component:HomeComponent},
+  {
+  path:"products",
+  component:ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,14 +11,21 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import {MatSidenavModule}  from'@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list'
+import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './view/home/home.component'
+import{MatCardModule} from'@angular/material/card';
+import { ProductCrudComponent } from './view/product-crud/product-crud.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+  
   ],
   imports: [
     BrowserModule,
@@ -26,7 +33,8 @@ import {MatListModule} from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
