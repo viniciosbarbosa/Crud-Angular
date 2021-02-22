d6f5ec748f29f116863bc7c5c437cfb53d7995c1
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2021-02-22 20:39:41 -0300

    fazendo form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,14 +9,19 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
-
 import {MatSidenavModule}  from'@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './view/home/home.component'
-import{MatCardModule} from'@angular/material/card';
+import {MatCardModule} from'@angular/material/card';
 import { ProductCrudComponent } from './view/product-crud/product-crud.component';
 import { ProductCreatComponent } from './components/product/product-creat/product-creat.component';
-import {MatButtonModule} from'@angular/material/button'
+import {MatButtonModule} from'@angular/material/button';
+import {MatSnackBarModule} from'@angular/material/snack-bar';
+import {HttpClientModule} from'@angular/common/http';
+import {FormsModule} from'@angular/forms'
+import {MatFormFieldModule} from'@angular/material/form-field'
+import {MatInputModule} from'@angular/material/input'
+
 
 
 
@@ -29,6 +34,7 @@ import {MatButtonModule} from'@angular/material/button'
     HomeComponent,
     ProductCrudComponent,
     ProductCreatComponent,
+    
   
   ],
   imports: [
@@ -39,7 +45,12 @@ import {MatButtonModule} from'@angular/material/button'
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,15 +1,29 @@
+import { Product } from './product.model';
 import { Injectable } from '@angular/core';
-import {} from'@angular/material/snack-bar'
+import {MatSnackBar} from'@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+   
+  baseUrl = "http://localhost:3001/products"
 
-  constructor() { }
+  constructor(private snackBar : MatSnackBar , private http :HttpClient) { }
 
-  showOnConsole(msg:string) : void{
-    console.log(msg)
+  showMessage(msg:string) : void{
+    this.snackBar.open(msg,'X',{
+      duration:3000,
+      horizontalPosition:'right',
+      verticalPosition:'top',
+
+    })
+  }
+
+  create(product: Product) :Observable<Product>{
+    return this.http.post<Product>(this.baseUrl,product)
   }
 
 }
