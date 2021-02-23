a184c9368e5ef6b875b2aec9655551ea9ad56203
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2021-02-22 22:25:31 -0300

    visualizando db

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,7 +20,8 @@ import {MatSnackBarModule} from'@angular/material/snack-bar';
 import {HttpClientModule} from'@angular/common/http';
 import {FormsModule} from'@angular/forms'
 import {MatFormFieldModule} from'@angular/material/form-field'
-import {MatInputModule} from'@angular/material/input'
+import {MatInputModule} from'@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 
 
@@ -34,6 +35,7 @@ import {MatInputModule} from'@angular/material/input'
     HomeComponent,
     ProductCrudComponent,
     ProductCreatComponent,
+    ProductReadComponent,
     
   
   ],
