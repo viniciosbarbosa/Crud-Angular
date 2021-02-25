6818cda794e62e283a074b8cfe788537c34e5375
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2021-02-25 01:47:15 -0300

    add

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,7 +21,11 @@ import {HttpClientModule} from'@angular/common/http';
 import {FormsModule} from'@angular/forms'
 import {MatFormFieldModule} from'@angular/material/form-field'
 import {MatInputModule} from'@angular/material/input';
-import { ProductReadComponent } from './components/product/product-read/product-read.component'
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
 
 
 
@@ -36,6 +40,7 @@ import { ProductReadComponent } from './components/product/product-read/product-
     ProductCrudComponent,
     ProductCreatComponent,
     ProductReadComponent,
+    ProductRead2Component,
     
   
   ],
@@ -53,6 +58,9 @@ import { ProductReadComponent } from './components/product/product-read/product-
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
